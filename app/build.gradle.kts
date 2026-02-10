89b26e3487dcfb25ba8d238f16d9b64e6e54acd4
eason204646 <eason204646@outlook.com>
2026-02-10 10:41:59 +0800

    更新版本号：versionCode 增至 14，versionName 保持 1.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.music.purelymusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.6.2"
+        versionCode = 14
+        versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
